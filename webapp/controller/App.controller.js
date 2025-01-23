c8b2a0b0987f6f01be1a65c7aa883f1233f37093
Trane20211 <hermes.demicheli@hotmail.com>
2025-01-23 11:18:09 +0100

    Step 4

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,90 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
-	"use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast",
+    "sap/ui/model/Sorter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/FilterType",
+  ],
+  function (
+    Controller,
+    JSONModel,
+    MessageBox,
+    MessageToast,
+    Sorter,
+    Filter,
+    FilterOperator,
+    FilterType
+  ) {
+    "use strict";
 
-	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
+    return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
+      /**
+       *  Hook for initializing the controller
+       */
+      onInit: function () {
+        var oJSONData = {
+            busy: false,
+            order: 0,
+          },
+          oModel = new JSONModel(oJSONData);
 
-		/**
-		 *  Hook for initializing the controller
-		 */
-		onInit : function () {
-			var oJSONData = {
-					busy : false
-				},
-				oModel = new JSONModel(oJSONData);
+        this.getView().setModel(oModel, "appView");
+      },
 
-			this.getView().setModel(oModel, "appView");
-		}
-	});
-});
+      onSearch: function () {
+        var oView = this.getView(),
+          sValue = oView.byId("searchField").getValue(),
+          oFilter = new Filter("LastName", FilterOperator.Contains, sValue);
+
+        oView
+          .byId("peopleList")
+          .getBinding("items")
+          .filter(oFilter, FilterType.Application);
+      },
+
+      onSort: function () {
+        var oView = this.getView(),
+          aStates = [undefined, "asc", "desc"],
+          aStateTextIds = ["sortNone", "sortAscending", "sortDescending"],
+          sMessage,
+          iOrder = oView.getModel("appView").getProperty("/order");
+
+        iOrder = (iOrder + 1) % aStates.length;
+        var sOrder = aStates[iOrder];
+
+        oView.getModel("appView").setProperty("/order", iOrder);
+        oView
+          .byId("peopleList")
+          .getBinding("items")
+          .sort(sOrder && new Sorter("LastName", sOrder === "desc"));
+
+        sMessage = this._getText("sortMessage", [
+          this._getText(aStateTextIds[iOrder]),
+        ]);
+        MessageToast.show(sMessage);
+      },
+
+      onRefresh: function () {
+        var oBinding = this.byId("peopleList").getBinding("items");
+
+        if (oBinding.hasPendingChange()) {
+          MessageBox.error(this._getText("refreshNotPossibleMessage"));
+          return;
+        }
+        oBinding.refresh();
+        MessageToast.show(this._getText("refreshSuccessMessage"));
+      },
+
+      _getText: function (sTextId, aArgs) {
+        return this.getOwnerComponent()
+          .getModel("i18n")
+          .getResourceBundle()
+          .getText(sTextId, aArgs);
+      },
+    });
+  }
+);
